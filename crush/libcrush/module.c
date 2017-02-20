32f5f67cbb841ec27c1a1bf58edaca9b71dbcf88
Loic Dachary <loic@dachary.org>
2017-02-20 17:23:05 +0100

    skeleton C module for libcrush
    Signed-off-by: Loic Dachary <loic@dachary.org>

---
+++ crush/libcrush/module.c
@@ -0,0 +1,77 @@
+#include <Python.h>
+
+#include "module.h"
+
+/* Module definition */
+
+#define MODULE_DOC PyDoc_STR("python wrapper for libcrush.")
+
+#if PY_MAJOR_VERSION >= 3
+
+static PyModuleDef
+moduledef = {
+    PyModuleDef_HEAD_INIT,
+    "crush.libcrush",
+    MODULE_DOC,
+    -1,
+    NULL,       /* methods */
+    NULL,
+    NULL,       /* traverse */
+    NULL,       /* clear */
+    NULL
+};
+
+
+PyObject *
+PyInit_tracer(void)
+{
+    PyObject * mod = PyModule_Create(&moduledef);
+    if (mod == NULL) {
+        return NULL;
+    }
+
+    if (LibCrush_intern_strings() < 0) {
+        return NULL;
+    }
+
+    /* Initialize LibCrush */
+    LibCrushType.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&LibCrushType) < 0) {
+        Py_DECREF(mod);
+        return NULL;
+    }
+
+    Py_INCREF(&LibCrushType);
+    if (PyModule_AddObject(mod, "LibCrush", (PyObject *)&LibCrushType) < 0) {
+        Py_DECREF(mod);
+        Py_DECREF(&LibCrushType);
+        return NULL;
+    }
+
+    return mod;
+}
+
+#else
+
+void
+inittracer(void)
+{
+    PyObject * mod;
+
+    mod = Py_InitModule3("crush.libcrush", NULL, MODULE_DOC);
+    if (mod == NULL) {
+        return;
+    }
+
+    /* Initialize LibCrush */
+    LibCrushType.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&LibCrushType) < 0) {
+        return;
+    }
+
+    Py_INCREF(&LibCrushType);
+    PyModule_AddObject(mod, "LibCrush", (PyObject *)&LibCrushType);
+
+}
+
+#endif /* Py3k */
---
+++ crush/libcrush/module.h
@@ -0,0 +1,13 @@
+#ifndef _MODULE_H
+#define _MODULE_H
+
+/* The LibCrush type. */
+
+typedef struct LibCrush {
+    PyObject_HEAD
+
+} LibCrush;
+
+extern PyTypeObject LibCrushType;
+
+#endif /* _MODULE_H */
