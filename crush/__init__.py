74ec438598a84cf1ef4c2281b505d1f1d993acd1
Loic Dachary <loic@dachary.org>
2017-02-24 15:51:47 +0100

    implement crush.Crush API
    Signed-off-by: Loic Dachary <loic@dachary.org>

---
+++ crush/__init__.py
@@ -0,0 +1,32 @@
+# -*- mode: python; coding: utf-8 -*-
+#
+# Copyright (C) 2017 <contact@redhat.com>
+#
+# Author: Loic Dachary <loic@dachary.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from crush.libcrush import LibCrush
+
+class Crush(object):
+
+    def __init__(self, verbose=False):
+        self.c = LibCrush(verbose and 1 or 0)
+
+    def parse(self, crushmap):
+        self.c.parse(crushmap)
+        return True
+
+    def map(self, *args, **kwargs):
+        return self.c.map(*args, **kwargs)
