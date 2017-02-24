e49990093f318a678fdd63c80c18752b83702b05
Loic Dachary <loic@dachary.org>
2017-02-25 00:50:04 +0100

    protect crush/__init__.py from import failure
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- crush/__init__.py
+++ crush/__init__.py
@@ -17,7 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from crush.libcrush import LibCrush
+try:  # chicken and egg problem when running pip install -e . from sources
+    from crush.libcrush import LibCrush
+except:
+    pass
 
 
 class Crush(object):
