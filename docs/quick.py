02dba190778e5d4f6f0d71d371ba4dc00b9737f8
Loic Dachary <loic@dachary.org>
2017-03-02 10:04:20 +0100

    update the quick start example
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- docs/quick.py
+++ docs/quick.py
@@ -3,26 +3,34 @@ from crush import Crush
 
 crushmap = """
 {
-  "trees": {
-    "dc1": {
-      "~type~": "root", "~id~": -1,
-      "host0": {
-        "~type~": "host", "~id~": -2,
-        "device0": { "~id~": 0, "~weight~": 1.0 },
-        "device1": { "~id~": 1, "~weight~": 2.0 }
-      },
-      "host1": {
-        "~type~": "host", "~id~": -3,
-        "device2": { "~id~": 2, "~weight~": 1.0 },
-        "device3": { "~id~": 3, "~weight~": 2.0 }
-      },
-      "host2": {
-        "~type~": "host", "~id~": -4,
-        "device4": { "~id~": 4, "~weight~": 1.0 },
-        "device5": { "~id~": 5, "~weight~": 2.0 }
-      }
+  "trees": [
+    {
+      "type": "root", "name": "dc1", "id": -1,
+      "children": [
+        {
+         "type": "host", "name": "host0", "id": -2,
+         "children": [
+          { "id": 0, "name": "device0", "weight": 1.0 },
+          { "id": 1, "name": "device1", "weight": 2.0 }
+         ]
+        },
+        {
+         "type": "host", "name": "host1", "id": -3,
+         "children": [
+          { "id": 2, "name": "device2", "weight": 1.0 },
+          { "id": 3, "name": "device3", "weight": 2.0 }
+         ]
+        },
+        {
+         "type": "host", "name": "host2", "id": -4,
+         "children": [
+          { "id": 4, "name": "device4", "weight": 1.0 },
+          { "id": 5, "name": "device5", "weight": 2.0 }
+         ]
+        }
+      ]
     }
-  },
+  ],
   "rules": {
     "data": [
       [ "take", "dc1" ],
