664119fbdf1d9847915a9c4cd4fc1fc4035706c1
Xavier Villaneau <xvillaneau@gmail.com>
2017-03-21 07:13:15 -0400

    tests: Added tests for straw algorithm support

--- tests/test_libcrush.py
+++ tests/test_libcrush.py
@@ -132,6 +132,25 @@ class TestLibCrush(object):
         with pytest.raises(TypeError) as e:
             LibCrush().parse(wrong)
 
+    def test_parse_straw_algorithm(self):
+        """Test that LibCrush only parses the straw algorithm
+        if backward_compatibility is set to True"""
+        straw_map = {
+            'trees': [{
+                'type': 'root',
+                'name': 'cluster',
+                'algorithm': 'straw'
+            }]
+        }
+
+        # Backward compatibility off: fails
+        with pytest.raises(RuntimeError) as e:
+            LibCrush().parse(straw_map)
+        assert 'not straw' in str(e.value)
+
+        # Backward compatibility on: it works
+        LibCrush(backward_compatibility=True).parse(straw_map)
+
     def test_parse_trees_invalid_key(self):
         wrong = {
             'trees': [{
