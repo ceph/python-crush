d183feefd4b8e742b6ce069f1c623bb5eb92c748
Xavier Villaneau <xvillaneau@gmail.com>
2017-03-25 13:29:01 -0400

    tests: Reverted un-necessary change

--- tests/test_crush.py
+++ tests/test_crush.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 import logging
-import pytest  # noqa needed for caplog
+import pytest # noqa needed for caplog
 
 from crush import Crush
 
