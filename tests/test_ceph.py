549d95cadca4e20b44289d3089d3abd80598aa5a
Loic Dachary <loic@dachary.org>
2017-03-26 10:32:55 +0200

    analyze,compare: implement --pool/--pg-num for Ceph
    As an alternative to analyzing using the [0,--values-count[ range, the
    --pool / --pg-num / --pgp-num arguments are used generate a list of
    values matching what Ceph uses when placing PGs.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- tests/test_ceph.py
+++ tests/test_ceph.py
@@ -57,6 +57,16 @@ class TestCeph(object):
             '--values-count', '2',
         ])
         assert {0: 0, 1: 1} == c.hook_create_values()
+        c.parse([
+            '--verbose',
+            'analyze',
+            '--pool', '2',
+            '--pg-num', '3',
+            '--pgp-num', '3',
+        ])
+        expected = {u'2.0': -113899774, u'2.1': -1215435108, u'2.2': -832918304}
+        assert expected == c.hook_create_values()
+
 # Local Variables:
 # compile-command: "cd .. ; virtualenv/bin/tox -e py27 -- -vv -s tests/test_ceph.py"
 # End:
