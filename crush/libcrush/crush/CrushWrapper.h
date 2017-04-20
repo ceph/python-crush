69bf33707c535440b411d3b08f5227d606691daa
Loic Dachary <loic@dachary.org>
2017-04-20 23:46:59 +0200

    libcrush: sync with Ceph
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- crush/libcrush/crush/CrushWrapper.h
+++ crush/libcrush/crush/CrushWrapper.h
@@ -58,6 +58,7 @@ public:
   std::map<int32_t, string> class_name; /* class id -> class name */
   std::map<string, int32_t> class_rname; /* class name -> class id */
   std::map<int32_t, map<int32_t, int32_t> > class_bucket; /* bucket[id][class] == id */
+  std::map<uint64_t, crush_choose_arg_map> choose_args;
 
 private:
   struct crush_map *crush;
@@ -87,6 +88,7 @@ public:
   ~CrushWrapper() {
     if (crush)
       crush_destroy(crush);
+    choose_args_clear();
   }
 
   crush_map *get_crush_map() { return crush; }
@@ -96,6 +98,7 @@ public:
     if (crush)
       crush_destroy(crush);
     crush = crush_create();
+    choose_args_clear();
     assert(crush);
     have_rmaps = false;
 
@@ -422,6 +425,18 @@ public:
     else
       return -EINVAL;
   }
+  int remove_class_name(const string& name) {
+    std::map<string,int>::const_iterator p = class_rname.find(name);
+    if (p == class_rname.end())
+      return -ENOENT;
+    int class_id = p->second;
+    std::map<int,string>::const_iterator q = class_name.find(class_id);
+    if (q == class_name.end())
+      return -ENOENT;
+    class_rname.erase(name);
+    class_name.erase(class_id);
+    return 0;
+  }
   int get_or_create_class_id(const string& name) {
     int c = get_class_id(name);
     if (c < 0) {
@@ -554,7 +569,7 @@ public:
    * FIXME: ambiguous for items that occur multiple times in the map
    */
   pair<string,string> get_immediate_parent(int id, int *ret = NULL);
-  int get_immediate_parent_id(int id, int *parent);
+  int get_immediate_parent_id(int id, int *parent) const;
 
   /**
    * get the fully qualified location of a device by successively finding
@@ -1091,6 +1106,7 @@ public:
 
   int update_device_class(CephContext *cct, int id, const string& class_name, const string& name);
   int device_class_clone(int original, int device_class, int *clone);
+  bool class_is_in_use(int class_id);
   int populate_classes();
   int rebuild_roots_with_classes();
   /* remove unused roots generated for class devices */
@@ -1159,19 +1175,73 @@ public:
     return result;
   }
 
+  crush_choose_arg_map choose_args_get(uint64_t choose_args_index) const {
+    auto i = choose_args.find(choose_args_index);
+    if (i == choose_args.end()) {
+      crush_choose_arg_map arg_map;
+      arg_map.args = NULL;
+      arg_map.size = 0;
+      return arg_map;
+    } else {
+      return i->second;
+    }
+  }
+
+  void destroy_choose_args(crush_choose_arg_map arg_map) {
+    for (__u32 i = 0; i < arg_map.size; i++) {
+      crush_choose_arg *arg = &arg_map.args[i];
+      for (__u32 j = 0; j < arg->weight_set_size; j++) {
+	crush_weight_set *weight_set = &arg->weight_set[j];
+	free(weight_set->weights);
+      }
+      if (arg->weight_set)
+	free(arg->weight_set);
+      if (arg->ids)
+	free(arg->ids);
+    }
+    free(arg_map.args);
+  }
+  
+  void choose_args_clear() {
+    for (auto w : choose_args)
+      destroy_choose_args(w.second);
+    choose_args.clear();
+  }
+
   void do_rule(int rule, int x, vector<int>& out, int maxout,
-	       const vector<__u32>& weight) const {
+	       const vector<__u32>& weight,
+	       uint64_t choose_args_index) const {
     int rawout[maxout];
     char work[crush_work_size(crush, maxout)];
     crush_init_workspace(crush, work);
+    crush_choose_arg_map arg_map = choose_args_get(choose_args_index);
     int numrep = crush_do_rule(crush, rule, x, rawout, maxout, &weight[0],
-			       weight.size(), work);
+			       weight.size(), work, arg_map.args);
     if (numrep < 0)
       numrep = 0;
     out.resize(numrep);
     for (int i=0; i<numrep; i++)
       out[i] = rawout[i];
   }
+
+  int _choose_type_stack(
+    CephContext *cct,
+    const vector<pair<int,int>>& stack,
+    const set<int>& overfull,
+    const vector<int>& underfull,
+    const vector<int>& orig,
+    vector<int>::const_iterator& i,
+    set<int>& used,
+    vector<int> *pw) const;
+
+  int try_remap_rule(
+    CephContext *cct,
+    int rule,
+    int maxout,
+    const set<int>& overfull,
+    const vector<int>& underfull,
+    const vector<int>& orig,
+    vector<int> *out) const;
 
   bool check_crush_rule(int ruleset, int type, int size,  ostream& ss) {
     assert(crush);
@@ -1205,6 +1275,7 @@ public:
   void dump_rules(Formatter *f) const;
   void dump_rule(int ruleset, Formatter *f) const;
   void dump_tunables(Formatter *f) const;
+  void dump_choose_args(Formatter *f) const;
   void list_rules(Formatter *f) const;
   void dump_tree(ostream *out, Formatter *f) const;
   void dump_tree(Formatter *f) const;
